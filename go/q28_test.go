08ea9e2d6bb8a9c353cd176cf0530d1edd3b76c9
spume <1291953408@qq.com>
2020-08-24 10:16:57 +0800

    [feat] q28 add kmp solution

--- go/q28_test.go
+++ go/q28_test.go
@@ -105,3 +105,63 @@ func Test_strStrInternal(t *testing.T) {
 		})
 	}
 }
+
+func Test_strStrKMP(t *testing.T) {
+	type args struct {
+		haystack string
+		needle   string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "strStr case 1",
+			args: args{
+				haystack: "hello",
+				needle:   "ll",
+			},
+			want: 2,
+		},
+		{
+			name: "strStr case 2",
+			args: args{
+				haystack: "aaaaa",
+				needle:   "bba",
+			},
+			want: -1,
+		},
+		{
+			name: "strStr case 3",
+			args: args{
+				haystack: "mississippi",
+				needle:   "sippia",
+			},
+			want: -1,
+		},
+		{
+			name: "strStr case 4",
+			args: args{
+				haystack: "",
+				needle:   "",
+			},
+			want: 0,
+		},
+		{
+			name: "strStr case 5",
+			args: args{
+				haystack: "hexllo",
+				needle:   "ll",
+			},
+			want: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := strStrKMP(tt.args.haystack, tt.args.needle); got != tt.want {
+				t.Errorf("strStrKMP() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
