bc728bbfe2cb5d5b2a70f53e11b55ea46a49bc59
spume <1291953408@qq.com>
2020-08-25 11:31:09 +0800

    [feat] 104. 二叉树的最大深度

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/gofromzero/leetcode
 
 go 1.14
-
-require golang.org/x/lint v0.0.0-20200302205851-738671d3881b // indirect
---
+++ go/q104.go
@@ -0,0 +1,15 @@
+package program
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	left := maxDepth(root.Left)
+	right := maxDepth(root.Right)
+
+	if left > right {
+		return left + 1
+	}
+	return right + 1
+}
