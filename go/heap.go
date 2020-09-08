cd2f94a4bcee649f659c8803b347b10ca96f4f86
spume <1291953408@qq.com>
2020-09-08 11:49:00 +0800

    [feat] format heap

--- go/heap.go
+++ go/heap.go
@@ -1,17 +1,23 @@
 package program
 
+// IntHeap heap []int
 type IntHeap []int
 
+// Len Len
 func (h IntHeap) Len() int { return len(h) }
 
+// Less Less
 func (h IntHeap) Less(i, j int) bool { return h[i] > h[j] }
 
+// Swap Swap
 func (h IntHeap) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
 
+// Push Push
 func (h *IntHeap) Push(x interface{}) {
 	*h = append(*h, x.(int))
 }
 
+// Pop Pop
 func (h *IntHeap) Pop() interface{} {
 	old := *h
 	n := len(old)
