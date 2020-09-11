c158cd416cc0410fa0ec8b9a3c37c8b000f91ad7
spume <1291953408@qq.com>
2020-09-11 11:16:23 +0800

    [feat] 94. 二叉树的中序遍历

--- go/q94.go
+++ go/q94.go
@@ -10,3 +10,24 @@ func inorderTraversal(root *TreeNode) []int {
 	result := append(left, root.Val)
 	return append(result, right...)
 }
+
+func inorderTraversal2(root *TreeNode) []int {
+	result := make([]int, 0)
+	if root == nil {
+		return result
+	}
+	arr := make([]*TreeNode, 0)
+	var head = root
+	for head != nil || len(arr) != 0 {
+		for head != nil {
+			arr = append(arr, head)
+			head = head.Left
+		}
+		head = arr[len(arr)-1]
+		arr = arr[:len(arr)-1]
+		result = append(result, head.Val)
+		head = head.Right
+	}
+
+	return result
+}
--- go/q94_test.go
+++ go/q94_test.go
@@ -42,3 +42,48 @@ func Test_inorderTraversal(t *testing.T) {
 		})
 	}
 }
+
+func Test_inorderTraversal2(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "inorderTraversal2",
+			args: args{
+				root: &TreeNode{
+					Val:  1,
+					Left: nil,
+					Right: &TreeNode{
+						Val: 2,
+						Left: &TreeNode{
+							Val:   3,
+							Left:  nil,
+							Right: nil,
+						},
+						Right: nil,
+					},
+				},
+			},
+			want: []int{1, 3, 2},
+		},
+		{
+			name: "inorderTraversal2",
+			args: args{
+				root: nil,
+			},
+			want: []int{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := inorderTraversal2(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("inorderTraversal2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
