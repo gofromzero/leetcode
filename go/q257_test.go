c27f76f2a1755b1b82dc8a423552400975ebb759
spume <1291953408@qq.com>
2020-09-25 10:57:45 +0800

    [feat] 257. 二叉树的所有路径

--- go/q257_test.go
+++ go/q257_test.go
@@ -50,6 +50,13 @@ func Test_binaryTreePaths(t *testing.T) {
 			},
 			want: []string{"1->2->5", "1->3"},
 		},
+		{
+			name: "binaryTreePaths",
+			args: args{
+				root: nil,
+			},
+			want: []string{},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
