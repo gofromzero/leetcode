8e342bfd5fc0b74dfbeb4cf52d377f8ec2a55935
spume <1291953408@qq.com>
2020-09-27 12:07:02 +0800

    [feat] 543. 二叉树的直径

--- go/q653_test.go
+++ go/q653_test.go
@@ -37,6 +37,31 @@ func Test_findTarget(t *testing.T) {
 			},
 			want: true,
 		},
+		{
+			name: "findTarget",
+			args: args{
+				root: &TreeNode{
+					Val: 5,
+					Left: &TreeNode{
+						Val: 3,
+						Left: &TreeNode{
+							Val: 2,
+						},
+						Right: &TreeNode{
+							Val: 4,
+						},
+					},
+					Right: &TreeNode{
+						Val: 6,
+						Right: &TreeNode{
+							Val: 7,
+						},
+					},
+				},
+				k: 3,
+			},
+			want: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
