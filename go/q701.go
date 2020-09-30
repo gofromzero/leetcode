dd5dc1bf03b48d7a1e4adab9f62e0221c6cf42bc
spume <19951030zxc>
2020-09-30 09:55:25 +0800

    [feat] 701. 二叉搜索树中的插入操作

---
+++ go/q701.go
@@ -0,0 +1,14 @@
+package program
+
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return &TreeNode{Val: val}
+	}
+	if root.Val <= val {
+		root.Right = insertIntoBST(root.Right, val)
+	} else {
+		root.Left = insertIntoBST(root.Left, val)
+	}
+
+	return root
+}
