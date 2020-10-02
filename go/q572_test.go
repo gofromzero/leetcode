9d51fb0a29b2a7dceb5f70ca5f3802a24ce3f0ff
spume <19951030zxc>
2020-10-02 22:01:55 +0800

    [feat] cover

--- go/q572_test.go
+++ go/q572_test.go
@@ -42,6 +42,76 @@ func Test_isSubtree(t *testing.T) {
 			},
 			want: true,
 		},
+		{
+			name: "isSubtree",
+			args: args{
+				s: &TreeNode{
+					Val: 3,
+					Left: &TreeNode{
+						Val: 4,
+						Left: &TreeNode{
+							Val: 1,
+						},
+						Right: &TreeNode{
+							Val: 2,
+						},
+					},
+					Right: &TreeNode{
+						Val: 5,
+					},
+				},
+				t: &TreeNode{
+					Val: 4,
+					Left: &TreeNode{
+						Val: 1,
+					},
+					Right: &TreeNode{
+						Val: 3,
+					},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "isSubtree",
+			args: args{
+				s: &TreeNode{
+					Val: 3,
+					Left: &TreeNode{
+						Val: 4,
+						Left: &TreeNode{
+							Val: 1,
+						},
+						Right: &TreeNode{
+							Val: 2,
+						},
+					},
+					Right: &TreeNode{
+						Val: 5,
+					},
+				},
+				t: &TreeNode{
+					Val: 4,
+					Left: &TreeNode{
+						Val: 1,
+					},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "isSubtree",
+			args: args{
+				s: nil,
+				t: &TreeNode{
+					Val: 4,
+					Left: &TreeNode{
+						Val: 1,
+					},
+				},
+			},
+			want: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
