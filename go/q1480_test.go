fb0e3a3a36a0d9ff9f5e131421ea816040503280
spume <1291953408@qq.com>
2021-01-27 15:36:20 +0800

    [feat] 1480. 一维数组的动态和

---
+++ go/q1480_test.go
@@ -0,0 +1,32 @@
+package program
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_runningSum(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "runningSum",
+			args: args{
+				nums: []int{1, 2, 3, 4},
+			},
+			want: []int{1, 3, 6, 10},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := runningSum(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("runningSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
