1bb02dde6b08329e5502298bf7c2fc350ae446b8
spume <1291953408@qq.com>
2022-05-25 10:27:44 +0800

    feat(problem): 1. twoSum

---
+++ algorithms/go/twoSum/twoSum.go
@@ -0,0 +1,17 @@
+// Copyright 2021 The GoLand Authors.
+// Author: Spume
+// Modified: 2022/5/25
+
+package twoSum
+
+func twoSum(nums []int, target int) []int {
+	var l = make(map[int]int)
+	for i, v := range nums {
+		if raw, ok := l[v]; ok {
+			return []int{i, raw}
+		}
+		l[target-v] = i
+	}
+	return nil
+
+}
