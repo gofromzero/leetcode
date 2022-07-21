4205f61acf2e2019e3a22bdd07d0aeae63d63039
spume <gofromzero@gmail.com>
2022-07-21 11:45:59 +0800

    feat(problem): 724. 寻找数组的中心下标

---
+++ algorithms/go/pivotIndex/pivotIndex_test.go
@@ -0,0 +1,36 @@
+package pivotIndex
+
+import "testing"
+
+func Test_pivotIndex(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "pivotIndex",
+			args: args{
+				nums: []int{1, 7, 3, 6, 5, 6},
+			},
+			want: 3,
+		},
+		{
+			name: "pivotIndex",
+			args: args{
+				nums: []int{1, 2, 3},
+			},
+			want: -1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := pivotIndex(tt.args.nums); got != tt.want {
+				t.Errorf("pivotIndex() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
